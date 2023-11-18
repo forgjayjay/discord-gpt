572622dad1043fe5063b916563bee45513b22f1f
forgjayjay <edlink2004@gmail.com>
2023-11-18 17:32:04 +0100

    fixed functionality and timeouts from the /gpt command

--- bot/gptapi.py
+++ bot/gptapi.py
@@ -1,20 +1,37 @@
 import os
+
 from openai import OpenAI
 from dotenv import load_dotenv
+from threading import Timer
 
 load_dotenv()
 KEY = os.getenv('OPENAI_API_KEY')
 
+
 client = OpenAI()
 client.api_key = KEY
 
+default_message = 'You are a chat bot in a discord server. Your task will be to answer questions given to you by it\'s members. '
+context = [default_message]
+
+def clear_context():
+    context.clear
+    context.append(default_message)
+
+timer = Timer(30 * 60, clear_context())
+
 def ask_gpt(arg):
+    if(timer.is_alive() == False):
+        timer.start()
+
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
         messages=[
-            {"role": "system", "content": "You are a chat bot in a discord server. Your task will be to answer questions given to you by it's members."},
-            {"role": "user", "content": arg}
+            {"role": "system", "content": "Here's the context so far: " + ' '.join(context)},
+            {"role": "user", "content": ' '.join(arg)}
         ]
     )
     response = completion.choices[0].message.content
+    context.append(response + ' ')
+    print(context)
     return response
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
-
 import discord
+import random
 
-from dotenv import load_dotenv
 from discord.ext import commands
+from dotenv import load_dotenv
 from bot.gptapi import ask_gpt
+from bot.emoticons import emoticons
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -14,18 +15,23 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='>', intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} is ready and online!')
-
 @bot.command(help='Sends a kaomoji symbolizing *Oh My God*')
 async def omg(ctx):
     print(f'sending kaomoji (๑>ᴗ<๑)')
     await ctx.send('(๑•́o•̀๑)')
 
+@bot.command(help='Sends a random kaomoji')
+async def kaomoji(ctx):
+    await ctx.send(random.choice(emoticons))
+
 @bot.slash_command(name='gpt', help='Asks a trusty gpt-3.5 with a given argument')
-async def gpt(ctx, arg):
-    response = ask_gpt(arg)
+async def gpt(ctx, argument):
+    await ctx.defer()
+    response = ask_gpt(argument)
     await ctx.respond(response)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} is ready and online!')
 
 bot.run(TOKEN)
